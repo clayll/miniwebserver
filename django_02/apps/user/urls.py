cdf036bbbfc8d95e80704ab7d45f6709c87cf7db
liuliang <8238491@163.com>
2019-02-23 14:03:54 +0800

    update

--- django_02/apps/user/urls.py
+++ django_02/apps/user/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import re_path
+from apps.user import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    # url(r'user/register_user$', views.register_user),
+    re_path(r'^register$', views.Register_view.as_view(), name='register'),
+    re_path(r'^active/(?P<id>.*)$', views.Active_view.as_view(), name='active'),
+    re_path(r'^login', views.Login_view.as_view(), name='login'),
+    re_path(r'^logout', views.Logout_View.as_view(), name='logout'),
+    re_path(r"^user", views.UserInfo_View.as_view(), name='user'),
+    re_path(r"^order", views.UserOrder_View.as_view(), name='order'),
+    re_path(r"^address", views.Address_View.as_view(), name='address')
 ]
