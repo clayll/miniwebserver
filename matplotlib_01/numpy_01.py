d334c8274006277f1fba2adc034d7a0921ee2cdb
liuliang <8238491@163.com>
2019-04-03 16:23:38 +0800

    update

--- matplotlib_01/numpy_01.py
+++ matplotlib_01/numpy_01.py
@@ -1,4 +1,21 @@
 import  numpy as np
 
-a = np.array([1,3,4])
-print(type(a))
+a = np.array([[1,3,4],[2,3,7]])
+# print(a)
+# b = np.ones((1,3),dtype=np.int)
+# print(b)
+
+
+
+
+c = np.array([[[1,3,4],[2,3,7]],[[1,2,3],[1,2,3]]])
+d = np.ones((1,3))
+
+
+x = a.transpose()
+print(x)
+print("*"*30)
+z = np.where(x<3,7,89)
+print(z)
+
+
