8cfd0f65a4c45c889033a2fe35a1ed9c5917fc48
liuliang <8238491@163.com>
2019-05-09 07:26:11 +0800

    update

--- MLearn_02/numpy_lineRegress.py
+++ MLearn_02/numpy_lineRegress.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import matplotlib as mpt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -9,24 +8,38 @@ data = np.genfromtxt("datas/data.csv",delimiter=",") #pd.read_csv("datas/data.cs
 x_data = data[:,0]
 y_data = data[:,1]
 k, b = 0.0, 0.0
-lr = 0.01
+lr = 0.0001
 m = len(x_data)
+grad_count = 50
+
+plt.scatter(x_data,y_data)
+plt.show()
 
 def createLossFuntion(k, b):
     loss = 0.0
     for i in range(m):
         loss += (y_data[i]-(k*x_data[i]+b))**2
 
-    return loss/m/2.0
+    return loss/float(m)/2.0
 
 def gradient_computer(k,b):
-    b_grad, k_grad =0.0 ,0.0
-    for i in range(m):
-        b_grad += (k*x_data[i]+b)  -y_data[i]
-        k_grad += ((k*x_data[i]+b)  -y_data[i])*x_data[i]
+    for j in range(grad_count):
+        b_grad, k_grad = 0.0, 0.0
+        for i in range(m):
+            # b_grad += ((k*x_data[i]+b)  -y_data[i])/m
+            # k_grad += (((k*x_data[i]+b)  -y_data[i])*x_data[i])/m
+            b_grad += (1 / m) * (((k * x_data[i]) + b) - y_data[i])
+            k_grad += (1 / m) * x_data[i] * (((k * x_data[i]) + b) - y_data[i])
 
-    k = k - lr*k_grad/m
-    b = b - lr*b_grad/m
+        b = b - (lr * b_grad)
+        k = k - (lr*k_grad)
+
+        if j % 5 == 0:
+            plt.plot(x_data,y_data,'b.')
+            plt.plot(x_data,x_data*k+b,'r')
+            plt.show()
+        print("第{0}次，k:{1}，b：{2}".format(j,k,b))
+
 
     return k,b
 
@@ -34,9 +47,8 @@ def gradient_computer(k,b):
 
 
 if __name__ == '__main__':
-
-    for i in range(50):
-        k,b = gradient_computer(k, b)
-        print("执行第{0}次，损失率为：{1},K：{2} b:{3}".format(i, createLossFuntion(k,b),k,b))
+    print("初始损失率：{0}".format(createLossFuntion(k, b)))
+    k,b = gradient_computer(k, b)
+    print("损失率为：{0},K：{1} b:{2}".format(createLossFuntion(k,b),k,b))
 
 
