b8d664c134daad3bfad7045a8bff2dd360cef3fb
liuliang <8238491@163.com>
2019-05-15 17:58:51 +0800

    update

---
+++ MLearn_02/单层感知器_神经网络.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Perceptron():
+    def __init__(self):
+        self.t = np.array([1,1,-1,-1]).reshape((4,1))
+        self.x_data =  np.array([[1,3,3],[1,4,3],[1,1,1],[1,0,2]])
+        self.lr = 0.11
+        self.ws  = (np.random.random([3,1])-0.5)*2
+
+
+
+
+    def update(self):
+        y = np.sign( np.dot(self.x_data, self.ws))
+
+        self.ws =self.ws + np.dot(self.x_data.T, ((self.t - y)*self.lr)) / self.x_data.shape[0]
+
+        return y
+    def run(self):
+        for i in range(100):
+            y = self.update()
+            if (self.t == y).all():
+                print('epoch:', i)
+                print(self.ws)
+                break
+    def showPlt(self):
+        plt.plot(self.x_data[:,1],self.x_data[:,2],'co')
+        print(np.dot(self.x_data,self.ws))
+        # plt.plot(self.x_data[:,1], np.dot(self.x_data,self.ws),"kx")
+        plt.show()
+
+    def prodict(self):
+        return
+
+if __name__ == '__main__':
+    p = Perceptron()
+    p.run()
+    p.showPlt()
--- MLearn_02/梯度下降法_非线性逻辑回归.py
+++ MLearn_02/梯度下降法_非线性逻辑回归.py
@@ -9,17 +9,19 @@ class PolynomialLogicRegression():
         self.x = data[:, 0:2]
         self.y = data[:, -1, np.newaxis]
         self.m = len(self.y)
+        self.epoch = 10000
+        self.ws = np.ones((3,1))
+        self.lr = 0.01
 
     # sigmoid，x大于0，则分类算1
     def sigmoid(self,x):
-
         return 1/(1+np.exp(-x))
 
     def caculateLoss(self,ws,x_data):
-        left = 0
-        right = 0
-        left = -np.log(x_data*ws)
-        right = -np.log(1-x_data*ws)
+
+        left = np.multiply(-self.y,np.log(x_data*ws))
+        right = np.multiply(-(1-self.y)*np.log(1-x_data*ws))
+        return  np.sum(left + right)/self.m
 
     def showPlotSource(self):
         x0,y0,x1,y1 = [],[],[],[]
@@ -37,11 +39,22 @@ class PolynomialLogicRegression():
 
     def prepareData(self):
         model = preprocessing.PolynomialFeatures()
-        x = np.array([[1,2],[2,3]])
-        print(model.fit_transform(x))
+        x_mat= model.fit_transform(self.x)
 
+        ws = np.ones((x_mat.shape[1],1))
+        for i in range(self.epoch):
+            # xMat和weights矩阵相乘
+            h = self.sigmoid(np.dot(x_mat, ws))
+
+            # 计算误差
+            ws_grad = np.dot(x_mat.T , (h - self.y)) / self.m
+
+            ws = ws - self.lr * ws_grad
+
+        return ws
 
 
 if __name__ == '__main__':
     poloynomial = PolynomialLogicRegression()
-    poloynomial.prepareData()
+    print(poloynomial.prepareData())
+
