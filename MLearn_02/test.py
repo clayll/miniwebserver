fc554e5a0957ab597d0a7123e12ea86d02d94b9a
liuliang <8238491@163.com>
2019-06-12 17:43:49 +0800

    update

--- MLearn_02/test.py
+++ MLearn_02/test.py
@@ -15,10 +15,18 @@ import numpy as np
 # sess = tf.Session()
 # print(sess.run(rst))
 
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-y= np.array([0,1,2])
-print(a[range(3),y])
+a = np.array([[1,3,1],[5,5,2],[15,8,9],[20,44,12]])
+# probs = a / np.sum(a, axis=1, keepdims=True)  # [N x K]
+# print(probs)
+y= np.array([0,1,0,1])
+# print(probs[range(4),y])
 
-a[range(3),y] -= 1
+probs = np.exp(a - np.max(a, axis=1, keepdims=True))
 
-print()
+probs /= np.sum(probs, axis=1, keepdims=True)
+loss = -np.sum(np.log(probs[np.arange(4), y])) / 4
+print(np.log(probs[np.arange(4), y]))
+print(loss)
+
+
+
