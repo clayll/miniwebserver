bd3a1d94fc004a908eea7e47ca2bd1d827f1df63
liuliang <8238491@163.com>
2019-06-17 22:33:18 +0800

    update

--- MLearn_02/tensorflow项目/NN_mnist_project.py
+++ MLearn_02/tensorflow项目/NN_mnist_project.py
@@ -17,19 +17,19 @@ x = tf.placeholder(shape=(None,784),dtype=tf.float32)
 y = tf.placeholder(shape=(None,10),dtype=tf.float32)
 
 
-w0 = tf.Variable(tf.zeros((784,784)),dtype=tf.float32)
-b0 = tf.Variable(tf.zeros([784]))
+# w0 = tf.Variable(tf.zeros((784,784)),dtype=tf.float32)
+# b0 = tf.Variable(tf.zeros([784]))
 
 w = tf.Variable(tf.zeros((784,10)),dtype=tf.float32)
 b = tf.Variable(tf.zeros([10]))
 
-# 第一层的结果
-y0_ = tf.matmul(x,w0)+b0
-
-corss_entroy0 = tf.nn.softmax(y0_)
+# # 第一层的结果
+# y0_ = tf.matmul(x,w0)+b0
+#
+# corss_entroy0 = tf.nn.softmax(y0_)
 
 # 第二层的结果
-y_ = tf.matmul(corss_entroy0,w)+b
+y_ = tf.matmul(x,w)+b
 
 corss_entroy = tf.nn.softmax(y_)
 
@@ -51,13 +51,13 @@ with tf.Session() as sess:
             break
         sess.run(train_step,feed_dict={x : x_train[start:end], y : y_train[start:end]})
         now_loss = sess.run(loss, feed_dict={x: x_train[start:end], y: y_train[start:end]})
-        print("s第%s轮训练后的now_loss值：%s " % (end, now_loss))
+        print("%s第%s轮训练后的now_loss值：%s " % (start,end, now_loss))
         if end %  2000 == 0 :
             print("准确率是：", sess.run(accuracy, feed_dict={x: x_train[start:end], y: y_train[start:end]}))
         start+= 500
         end +=  500
-    y_prodict = sess.run(corss_entroy,feed_dict={x: x_test,y: y_test})
-    print("准确率是：", sess.run(accuracy, feed_dict={x: x_test,y: y_test}))
+    # y_prodict = sess.run(corss_entroy,feed_dict={x: x_test,y: y_test})
+    # print("准确率是：", sess.run(accuracy, feed_dict={x: x_test,y: y_test}))
     #
     # for i in range(1000):
     #     batch_xs, batch_ys = ds.train.next_batch(500)
