164dfeabfd42b0a76ecab4ed5ee25138f7d04a14
liuliang <8238491@163.com>
2019-07-16 17:47:48 +0800

    update

--- MLearn_02/test.py
+++ MLearn_02/test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import tensorflow as tf
+
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 # index = ['a','b']
@@ -14,36 +16,68 @@ import numpy as np
 # rst = tf.multiply(a,b)
 # sess = tf.Session()
 # print(sess.run(rst))
+#
+# a = tf.constant(123,dtype=tf.int32)
+# b = tf.constant(4,dtype=tf.int32)
+# rst = tf.add(a,b)
+# rst = tf.multiply(a,b)
+# sess = tf.Session()
+# print(sess.run(rst))
+#
+#
+# np.random.normal()
+#
+# a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+# y= np.array([0,1,2])
+# print(a[range(3),y])
+#
+# a = np.array([[1,3,1],[5,5,2],[15,8,9],[20,44,12]])
+# # probs = a / np.sum(a, axis=1, keepdims=True)  # [N x K]
+# # print(probs)
+# y= np.array([0,1,0,1])
+# # print(probs[range(4),y])
+#
+# probs = np.exp(a - np.max(a, axis=1, keepdims=True))
+#
+# probs /= np.sum(probs, axis=1, keepdims=True)
+# loss = -np.sum(np.log(probs[np.arange(4), y])) / 4
+# print(np.log(probs[np.arange(4), y]))
+# print(loss)
+# s = tf.truncated_normal((784,500),mean=0.1,stddev=0.1)
+# sess = tf.Session()
+# print(sess.run(s))
 
-a = tf.constant(123,dtype=tf.int32)
-b = tf.constant(4,dtype=tf.int32)
-rst = tf.add(a,b)
-rst = tf.multiply(a,b)
-sess = tf.Session()
-print(sess.run(rst))
+# 合并多个csv
+def pd_tocsv():
+    pdread1 = pd.read_csv(r"C:\Users\刘靓\Desktop\xa.txt",delimiter='\t',encoding='utf_8_sig')
+    pdread1["city"] = '西安'
+    pdread2 = pd.read_csv(r"C:\Users\刘靓\Desktop\wh.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread2["city"] = '武汉'
+    pdread3 = pd.read_csv(r"C:\Users\刘靓\Desktop\bj.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread3["city"] = '北京'
 
+    pdread4 = pd.read_csv(r"C:\Users\刘靓\Desktop\cd.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread4["city"] = '成都'
 
-np.random.normal()
+    pdread5 = pd.read_csv(r"C:\Users\刘靓\Desktop\gz.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread5["city"] = '广州'
 
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-y= np.array([0,1,2])
-print(a[range(3),y])
+    pdread6 = pd.read_csv(r"C:\Users\刘靓\Desktop\hz.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread6["city"] = '杭州'
 
-a = np.array([[1,3,1],[5,5,2],[15,8,9],[20,44,12]])
-# probs = a / np.sum(a, axis=1, keepdims=True)  # [N x K]
-# print(probs)
-y= np.array([0,1,0,1])
-# print(probs[range(4),y])
+    pdread7 = pd.read_csv(r"C:\Users\刘靓\Desktop\sh.txt", delimiter='\t', encoding='utf_8_sig')
+    pdread7["city"] = '上海'
 
-probs = np.exp(a - np.max(a, axis=1, keepdims=True))
+    pdread = pd.concat([pdread1,pdread2,pdread3,pdread4,pdread5,pdread6,pdread7],axis=0)
 
-probs /= np.sum(probs, axis=1, keepdims=True)
-loss = -np.sum(np.log(probs[np.arange(4), y])) / 4
-print(np.log(probs[np.arange(4), y]))
-print(loss)
-s = tf.truncated_normal((784,500),mean=0.1,stddev=0.1)
-sess = tf.Session()
-print(sess.run(s))
+    pdread.to_csv(r'C:\Users\刘靓\Desktop\数据分析_词频.csv', header=True, index=False,encoding='utf_8_sig')
 
-
+# 测试标准化
+def testStander():
+    np.random.seed(2)
+    randrage =  np.random.randint(1,100,[10,1]).astype(dtype=np.float)
+    print(randrage[:,0:1])
+    randrage1 = StandardScaler()
+    print(randrage1.fit(randrage[:,0:1]))
+testStander()
 
--- spider_02/boss/bossRecruit/bossRecruit/spiders/boss.py
+++ spider_02/boss/bossRecruit/bossRecruit/spiders/boss.py
@@ -9,49 +9,24 @@ import csv
 class BossSpider(scrapy.Spider):
     name = 'boss'
     allowed_domains = ['zhipin.com']
-    start_urls = ['https://www.zhipin.com/job_detail/?query=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&city=101270100&industry=&position=']
-    base_url = 'http://www.zhipin.com/'
+    # start_urls = ['https://www.zhipin.com/job_detail/?query=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&city=101270100&industry=&position=']
+    # base_url = 'http://www.zhipin.com/'
+    start_urls = 'https://www.lagou.com/jobs/list_%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90?px=default&city=%E6%88%90%E9%83%BD#filterBox'
+    base_url = 'https://www.lagou.com/'
     base_city = '成都'
 
     def parse(self, response):
 
-        lis = response.xpath("//div[@id='main']//ul/li")
+        # item = self.get_boss_list(response)
+        item = self.get_lagou_list(response)
 
-        for li in lis:
-            # price = li.xpath(".//div[@class='res-info']/p[@class='prive-tag']").extract_first()
-            href = self.base_url+li.xpath("//div[@class='job-primary']//a/@href").extract_first()
-            title = li.xpath("//div[@class='job-primary']//a/div[@class='job-title']/text()").extract_first()
-            item = dict({ "boss链接": href,"职位名称": title})
-            time.sleep(random.random() * 10)
-            yield scrapy.Request(href, callback=self.parse_details ,meta={"item": item}, dont_filter=True)
 
-        nexturl = self.base_url+response.xpath("//a[@ka='page-next']/@href").extract_first()
-        while nexturl:
-            print(nexturl)
-            yield scrapy.Request(nexturl, callback=self.parse)
 
         # yield scrapy.Request(nexturl, callback=self.parse)
 
     def parse_details(self, response):
 
         item = response.meta["item"]
-        item["城市"] = self.base_city
-        item["薪资"] = response.xpath("//span[@class='salary']/text()").extract_first()
-        item["学历要求"] = self.deal_em(response.xpath("//div[@class='job-banner']//p").extract())
-        item["岗位福利"] = self.deal_with_dot(response.xpath("//div[@class='job-banner']//div[@class='job-tags']/span"))
-        item["工作描述"] = self.deal_space_lst(response.xpath("//div[@class='detail-content']//div[@class='job-sec'][1]/div[@class='text']/text()").extract())
-
-        # item["团队介绍"] = self.deal_with_dot(response.xpath(
-        #     "//div[@class='detail-content']//div[@class='job-sec'][2]/div[@class='job-tags']/span"))
-        item["公司福利"] = self.deal_with_dot(response.xpath("//div[@class='detail-content']//div[@class='job-sec'][2]/div[@class='job-tags']/span"))
-        item["公司名称"] = response.xpath("//div[@class='sider-company']//a[@ka='job-detail-company_custompage']/text()").extract_first()
-        item["融资情况"] = response.xpath(
-            "//div[@class='sider-company']//i[@class='icon-stage']/../text()").extract_first()
-        item["公司规模"] = response.xpath(
-            "//div[@class='sider-company']//i[@class='icon-scale']/../text()").extract_first()
-        item["公司行业"] = response.xpath(
-            "//div[@class='sider-company']//a[@ka='job-detail-brandindustry']/text()").extract_first()
-        item["公司地址"] = response.xpath("//div[@class='location-address']/text()").extract_first()
 
 
 
@@ -81,3 +56,55 @@ class BossSpider(scrapy.Spider):
         elements = element.split('<em class="dolt"></em>')
 
         return elements
+
+    def get_boss_detals(self ,response,item):
+        item["城市"] = self.base_city
+        item["薪资"] = response.xpath("//span[@class='salary']/text()").extract_first()
+        item["学历要求"] = self.deal_em(response.xpath("//div[@class='job-banner']//p").extract())
+        item["岗位福利"] = self.deal_with_dot(response.xpath("//div[@class='job-banner']//div[@class='job-tags']/span"))
+        item["工作描述"] = self.deal_space_lst(response.xpath(
+            "//div[@class='detail-content']//div[@class='job-sec'][1]/div[@class='text']/text()").extract())
+
+        # item["团队介绍"] = self.deal_with_dot(response.xpath(
+        #     "//div[@class='detail-content']//div[@class='job-sec'][2]/div[@class='job-tags']/span"))
+        item["公司福利"] = self.deal_with_dot(
+            response.xpath("//div[@class='detail-content']//div[@class='job-sec'][2]/div[@class='job-tags']/span"))
+        item["公司名称"] = response.xpath(
+            "//div[@class='sider-company']//a[@ka='job-detail-company_custompage']/text()").extract_first()
+        item["融资情况"] = response.xpath(
+            "//div[@class='sider-company']//i[@class='icon-stage']/../text()").extract_first()
+        item["公司规模"] = response.xpath(
+            "//div[@class='sider-company']//i[@class='icon-scale']/../text()").extract_first()
+        item["公司行业"] = response.xpath(
+            "//div[@class='sider-company']//a[@ka='job-detail-brandindustry']/text()").extract_first()
+        item["公司地址"] = response.xpath("//div[@class='location-address']/text()").extract_first()
+
+    def get_boss_list(self,response):
+        lis = response.xpath("//div[@id='main']//ul/li")
+
+        for li in lis:
+            # price = li.xpath(".//div[@class='res-info']/p[@class='prive-tag']").extract_first()
+            href = self.base_url+li.xpath("//div[@class='job-primary']//a/@href").extract_first()
+            title = li.xpath("//div[@class='job-primary']//a/div[@class='job-title']/text()").extract_first()
+            item = dict({ "boss链接": href,"职位名称": title})
+            time.sleep(random.random() * 10)
+            yield scrapy.Request(href, callback=self.parse_details, meta={"item": item}, dont_filter=True)
+        nexturl = self.base_url + response.xpath("//a[@ka='page-next']/@href").extract_first()
+        while nexturl:
+            print(nexturl)
+            yield scrapy.Request(nexturl, callback=self.parse)
+
+    def get_lagou_list(self,response):
+        lis = response.xpath("//a[@class='position_link']")
+
+        for li in lis[0:2]:
+
+            href = li.xpath("./@href").extract_first()
+            title = li.xpath("./text()").extract_first()
+            item = dict({"链接": href, "职位名称": title})
+            time.sleep(random.random() * 10)
+            yield scrapy.Request(href, callback=self.parse_details, meta={"item": item}, dont_filter=True)
+        nexturl = self.base_url + response.xpath("//a[@ka='page-next']/@href").extract_first()
+        while nexturl:
+            print(nexturl)
+            yield scrapy.Request(nexturl, callback=self.parse)
